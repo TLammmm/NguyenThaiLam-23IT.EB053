7d15137471384e05f75fe4f6274bf775a09ab07a
TLammmm <160579015+TLammmm@users.noreply.github.com>
2024-04-13 23:39:55 +0700

    Main.java

--- baitap/src/application/Main.java
+++ baitap/src/application/Main.java
@@ -10,25 +10,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load file FXML
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/directoryTree.fxml"));
         Parent root = loader.load();
-
-        // Lấy controller từ FXMLLoader
-
-        // Thiết lập sự kiện đóng ứng dụng
         primaryStage.setOnCloseRequest(event -> {
-            // Thực hiện các thao tác cần thiết trước khi đóng ứng dụng (nếu có)
         });
-
-        // Thiết lập scene và hiển thị stage
         Scene scene = new Scene(root);
         primaryStage.setTitle("TreeView Sample");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
-
     public static void main(String[] args) {
         launch(args);
     }
